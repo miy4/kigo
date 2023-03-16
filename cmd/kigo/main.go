0c7113bb56ec4993b5d2ab7436c5a7d6f15bd766
miy4 <miy4@users.noreply.github.com>
2023-03-17 00:20:45 +0900

    Step 3: read keypresses from the user

--- cmd/kigo/main.go
+++ cmd/kigo/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "os"
+import (
+	"io"
+	"os"
+)
 
 func main() {
+	b := make([]byte, 1)
+	for {
+		_, err := os.Stdin.Read(b)
+		if err == io.EOF {
+			break
+		}
+	}
+
 	os.Exit(0)
 }
