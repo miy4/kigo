0a524f4ac2a0c616a08cbd17027d2054d5e6614a
miy4 <miy4@users.noreply.github.com>
2023-03-17 22:46:51 +0900

    Step 21: refactor keyboard input

---
+++ editor.go
@@ -0,0 +1,48 @@
+package kigo
+
+import (
+	"errors"
+)
+
+var Quit = errors.New("Quit")
+
+type Editor struct {
+	term *Terminal
+}
+
+func NewEditor() *Editor {
+	term := NewTerminal()
+	return &Editor{term}
+}
+
+func (editor *Editor) processKeypress() error {
+	key, err := editor.term.ReadKey()
+	if err != nil {
+		return err
+	}
+
+	switch key {
+	case KeyCtrlQ:
+		return Quit
+	}
+
+	return nil
+}
+
+func (editor *Editor) Run() error {
+	if err := editor.term.EnableRawMode(); err != nil {
+		return err
+	}
+	defer editor.term.DisableRawMode()
+
+	for {
+		err := editor.processKeypress()
+		if err == Quit {
+			break
+		} else if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ term.go
@@ -0,0 +1,71 @@
+package kigo
+
+import (
+	"io"
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+type Terminal struct {
+	org *unix.Termios
+	in  *os.File
+	out *os.File
+}
+
+func NewTerminal() *Terminal {
+	return &Terminal{
+		in:  os.Stdin,
+		out: os.Stdout,
+	}
+}
+
+func (term *Terminal) EnableRawMode() error {
+	raw, err := unix.IoctlGetTermios(int(term.in.Fd()), unix.TCGETS)
+	if err != nil {
+		return err
+	}
+
+	org := *raw
+	term.org = &org
+
+	raw.Iflag &^= unix.BRKINT | unix.IXON | unix.INPCK | unix.ISTRIP | unix.ICRNL
+	raw.Oflag &^= unix.OPOST
+	raw.Cflag |= unix.CS8
+	raw.Lflag &^= unix.ECHO | unix.ICANON | unix.IEXTEN | unix.ISIG
+	raw.Cc[unix.VMIN] = 0
+	raw.Cc[unix.VTIME] = 1
+
+	err = unix.IoctlSetTermios(int(term.in.Fd()), unix.TCSETSF, raw)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (term *Terminal) DisableRawMode() error {
+	if term.org == nil {
+		return nil
+	}
+
+	err := unix.IoctlSetTermios(int(term.in.Fd()), unix.TCSETSF, term.org)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (term *Terminal) ReadKey() (Key, error) {
+	b := make([]byte, 1)
+	for {
+		n, err := term.in.Read(b)
+		if err != nil && err != io.EOF {
+			return 0, err
+		} else if n == 1 {
+			break
+		}
+	}
+	return Key(b[0]), nil
+}
