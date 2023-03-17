26d0f4d9f3246f916f35aea3c1a38f0cd557ec5b
miy4 <miy4@users.noreply.github.com>
2023-03-17 12:35:02 +0900

    Step 5: turn off echoing

--- cmd/kigo/main.go
+++ cmd/kigo/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"io"
 	"os"
+
+	"golang.org/x/sys/unix"
 )
 
+func enableRawMode() {
+	raw, _ := unix.IoctlGetTermios(int(os.Stdin.Fd()), unix.TCGETS)
+	raw.Lflag &^= unix.ECHO
+	unix.IoctlSetTermios(int(os.Stdin.Fd()), unix.TCSETSF, raw)
+}
+
 func main() {
+	enableRawMode()
+
 	b := make([]byte, 1)
 	for {
 		_, err := os.Stdin.Read(b)
