c5451fa62a930c2575d44abcfe269d6225cd8538
miy4 <miy4@users.noreply.github.com>
2023-03-18 18:19:53 +0900

    Step 41-42: welcome message

--- editor.go
+++ editor.go
@@ -2,7 +2,11 @@ package kigo
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 )
+
+const version = "0.1.0"
 
 var Quit = errors.New("Quit")
 
@@ -25,12 +29,33 @@ func (editor *Editor) cols() int {
 
 func (editor *Editor) drawRows() {
 	for y := 0; y < editor.rows(); y++ {
-		editor.term.writeString("~")
+		if y == editor.rows()/3 {
+			editor.drawWelcome()
+		} else {
+			editor.term.writeString("~")
+		}
+
 		editor.term.clearLineRight()
+
 		if y < int(editor.rows()) {
 			editor.term.writeString("\r\n")
 		}
 	}
+}
+
+func (editor *Editor) drawWelcome() {
+	welcome := fmt.Sprintf("Kigo editor -- version %s", version)
+	if len(welcome) > editor.cols() {
+		welcome = welcome[:editor.cols()]
+	}
+
+	padding := (editor.cols() - len(welcome)) / 2
+	if padding > 0 {
+		editor.term.writeString("~")
+		editor.term.writeString(strings.Repeat(" ", padding-1))
+	}
+
+	editor.term.writeString(welcome)
 }
 
 func (editor *Editor) refreshScreen() {
